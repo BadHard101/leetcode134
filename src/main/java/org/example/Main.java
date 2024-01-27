bfb2c6baacd899ed032e3bf355b71b663072d017
BadHard <BadHard101@yandex.ru>
2024-01-27 06:40:45 +0300

    +try

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,40 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
+        Queue<Integer> starts = new LinkedList<>();
+        for (int i = 0; i < gas.length; i++) {
+            if (gas[i] - cost[i] >= 0)
+                starts.add(i);
+        }
+
+        if (starts.isEmpty()) return -1;
+
+        while (!starts.isEmpty()) {
+            int start = starts.poll();
+            int index = start;
+            int tank = 0;
+            while (tank + gas[index] - cost[index] >= 0) {
+                tank += gas[index];
+                tank -= cost[index];
+                index++;
+                if (index == gas.length)
+                    index = 0;
+                if (index == start)
+                    return start;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("ANS: " + canCompleteCircuit(
+                new int[]{1,2,3,4,5},
+                new int[]{3,4,5,1,2}
+        ));
     }
 }
